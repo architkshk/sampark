ff2d09eeae42300da6f691dd2814d33959d86776
Archit Kaushik <asarchit65@gmail.com>
2019-03-30 03:12:02 +0530

    Add mongoose

---
+++ server/config/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGO_URI:
+    "mongodb+srv://admin:admin@cluster0-1aqsa.mongodb.net/test?retryWrites=true"
+};
--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-var app = express();
+let app = express();
 app.use(cors());
 app.use(bodyParser.json());
+
+const { mongoose } = require("./db/");
 
 app.listen(5000, () => {
   console.log("Sever running on port 5000");
