98012a5e9f2202e76b974e8a128b44c5555f6113
Archit Kaushik <asarchit65@gmail.com>
2019-03-30 17:00:37 +0530

    Pwa setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDom from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from './components/App'
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDom.render(
-    <App />,
-  document.getElementById("root"));
+ReactDom.render(<App />, document.getElementById("root"));
+serviceWorker.register();
